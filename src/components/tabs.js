48f69c57c29e0f71cd9a016ae022c4c921595e0c
enhopkin@gmail.com <enhopkin@gmail.com>
2022-02-28 14:12:40 -0700

    complete. directions for tabs were misleading

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -21,35 +21,35 @@ const Tabs = (topics) => {
   const tab1= document.createElement('div');
   const tab2 =document.createElement('div');
   const tab3 = document.createElement('div')
-  // const tab4 = document.createElement('div')
-  // const tab5 = document.createElement('div')
+  const tab4 = document.createElement('div')
+  const tab5 = document.createElement('div')
 
   topicDiv.appendChild(tab1);
   topicDiv.appendChild(tab2)
   topicDiv.appendChild(tab3);
-  // topicDiv.appendChild(tab4);
-  // topicDiv.appendChild(tab5)
+  topicDiv.appendChild(tab4);
+  topicDiv.appendChild(tab5)
 
   topicDiv.classList.add('topics');
   tab1.classList.add('tab');
   tab2.classList.add('tab');
   tab3.classList.add('tab');
-  // tab4.classList.add('tab');
-  // tab5.classList.add('tab');
+  tab4.classList.add('tab');
+  tab5.classList.add('tab');
 
   
   tab1.textContent= topics[0];
   tab2.textContent= topics[1];
   tab3.textContent= topics[2];
-  // tab4.textContent = topics[3];
-  // tab5.textContent= topics[4];
+  tab4.textContent = topics[3];
+  tab5.textContent= topics[4];
 
   return topicDiv
 }
 
 
-const selector= '.tabs-container'
-const tabsEntry= document.querySelector(selector)
+
+const tabsEntry= document.querySelector('.tabs-container')
 
 const tabsAppender = (selector) => {
   // TASK 4
@@ -61,25 +61,30 @@ const tabsAppender = (selector) => {
   //
 axios.get(`http://localhost:5000/api/topics`)
   .then( resp =>{
-    const topicsOrigin = resp.data.topics;
+    const topics = resp.data.topics;
 
-    function shuffle(array) {
-      let currentIndex=array.length,   randomIndex;
-      while (currentIndex!=0) {
-randomIndex = Math.floor(Math.random() * currentIndex);
-currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
-}
-return array
-      }
-    const topics = shuffle(topicsOrigin)
+
+//     function shuffle(array) {
+//       let currentIndex=array.length,   randomIndex;
+//       while (currentIndex!=0) {
+// randomIndex = Math.floor(Math.random() * currentIndex);
+// currentIndex--;
+//       [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+// }
+// return array
+//       }
+//     const topics = shuffle(topicsOrigin)
     
-    // console.log(topics)
-    const tabBuilder = Tabs(topics);
-    // const tabsBase= document.querySelector('.tabs-container');
-    // tabsBase.appendChild(tabBuilder)
-    document.querySelector('.tabs-container').appendChild(tabBuilder)
-  })
+
+    console.log(topics)
+    // const tabBuilder = Tabs(topics);
+    // for( let i=0; i<topics.length; i++) {
+      
+      document.querySelector(selector).appendChild(Tabs(topics))
+    // }
+    })
+    
+
   .catch( err => {
     console.error(err)
   })
